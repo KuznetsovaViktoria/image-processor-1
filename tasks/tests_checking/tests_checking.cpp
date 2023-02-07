37a05783b183e14009a1ad6a15ea814ec525fcc9
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-07 11:12:51 +0300

    tests_checking done

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,19 @@
 #include "tests_checking.h"
+#include <vector>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::vector<std::string> ans;
+    std::vector<std::string> order;
+    for (auto& act : student_actions) {
+        if (act.side == Side::Top) {
+            order.insert(order.begin(), act.name);
+        } else {
+            order.push_back(act.name);
+        }
+    }
+    for (size_t x : queries) {
+        ans.push_back(order[x - 1]);
+    }
+    return ans;
 }
