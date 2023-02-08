43498804d95e286c15cc045f270cf382e4e27aea
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-08 23:48:46 +0300

    admission code style

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -4,17 +4,17 @@
 #include <unordered_map>
 
 AdmissionTable FillUniversities(const std::vector<University> &universities, const std::vector<Applicant> &applicants) {
-    std::vector <const Applicant*> sorted_applicants;
-    for (const auto& person : applicants){
+    std::vector<const Applicant *> sorted_applicants;
+    for (const auto &person : applicants) {
         sorted_applicants.push_back(&person);
     }
-    std::sort(sorted_applicants.begin(), sorted_applicants.end(), [](const Applicant* a, const Applicant* b) -> bool {
+    std::sort(sorted_applicants.begin(), sorted_applicants.end(), [](const Applicant *a, const Applicant *b) -> bool {
         const int minus_a_points = -a->points;
         const int minus_b_points = -b->points;
-        return std::tie(minus_a_points, a->student.birth_date.year, a->student.birth_date.month, a->student.birth_date.day,
-                        a->student.name) < std::tie(minus_b_points, b->student.birth_date.year,
-                                                   b->student.birth_date.month, b->student.birth_date.day,
-                                                   b->student.name);
+        return std::tie(minus_a_points, a->student.birth_date.year, a->student.birth_date.month,
+                        a->student.birth_date.day, a->student.name) <
+               std::tie(minus_b_points, b->student.birth_date.year, b->student.birth_date.month,
+                        b->student.birth_date.day, b->student.name);
     });
     std::unordered_map<std::string, size_t> universities_map;
     for (const auto &uni : universities) {
