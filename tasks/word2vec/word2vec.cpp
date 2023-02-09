8177da613b8427c92d3b14bdcc2e5e1bcee0af08
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-09 10:57:19 +0300

    word2vec done

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,8 +1,30 @@
 #include "word2vec.h"
-
+#include <string>
 #include <vector>
+
+size_t count_product(const std::vector<int>& a, const std::vector<int>& b) {
+    size_t res = 0;
+    for (size_t i = 0; i < a.size(); ++i) {
+        res += a[i] * b[i];
+    }
+    return res;
+}
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+    if (words.size() <= 1) {
+        return {};
+    }
+    std::vector<std::string> ans;
+    size_t mx = count_product(vectors[0], vectors[1]);
+    for (size_t i = 2; i < words.size(); ++i) {
+        size_t cnt = count_product(vectors[0], vectors[i]);
+        if (cnt > mx) {
+            mx = cnt;
+            ans = {words[i]};
+        } else if (cnt == mx) {
+            ans.push_back(words[i]);
+        }
+    }
+    return ans;
 }
