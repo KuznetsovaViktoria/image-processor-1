486344ecb275028053d21475bef586cd726d7ec3
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-09 15:50:35 +0300

    scorer done

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,47 @@
 #include "scorer.h"
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    return {}; // test
+    std::map<std::string, std::set<std::string>> ans;
+    std::map<std::string, std::map<std::string, std::vector<Event>>> event_list;
+    std::vector<std::string> people;
+    std::vector<std::string> tasks;
+    for (const Event& event : events) {
+        if (event_list[event.student_name].empty()) {
+            people.push_back(event.student_name);
+        }
+        if (event_list[event.student_name][event.task_name].empty()) {
+            tasks.push_back(event.task_name);
+        }
+        event_list[event.student_name][event.task_name].push_back(event);
+    }
+    for (const std::string& person : people) {
+        for (const std::string& task : tasks) {
+            std::sort(event_list[person][task].begin(), event_list[person][task].end(),
+                      [](const Event& a, const Event& b) -> bool { return a.time < b.time; });
+            bool if_ci_success = false;
+            bool if_merge_closed = true;
+            for (const Event& event : event_list[person][task]) {
+                if (event.time > score_time) {
+                    break;
+                }
+                if (event.event_type == EventType::CheckFailed) {
+                    if_ci_success = false;
+                } else if (event.event_type == EventType::CheckSuccess) {
+                    if_ci_success = true;
+                } else if (event.event_type == EventType::MergeRequestOpen) {
+                    if_merge_closed = false;
+                } else {
+                    if_merge_closed = true;
+                }
+            }
+            if (if_ci_success && if_merge_closed) {
+                ans[person].insert(task);
+            }
+        }
+    }
+    return ans;
 }
