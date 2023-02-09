a3600baacc1837c1b3c100d4fb80bc71b36bfc33
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-09 16:36:37 +0300

    scorer optimised

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -16,30 +16,33 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         if (event_list[event.student_name][event.task_name].empty()) {
             tasks.push_back(event.task_name);
         }
-        event_list[event.student_name][event.task_name].push_back(event);
+        if (event.time <= score_time) {
+            event_list[event.student_name][event.task_name].push_back(event);
+        }
     }
     for (const std::string& person : people) {
         for (const std::string& task : tasks) {
             std::sort(event_list[person][task].begin(), event_list[person][task].end(),
-                      [](const Event& a, const Event& b) -> bool { return a.time < b.time; });
+                      [](const Event& a, const Event& b) -> bool { return b.time < a.time; });
             bool if_ci_success = false;
-            bool if_merge_closed = true;
+            bool if_merge_closed = false;
             for (const Event& event : event_list[person][task]) {
-                if (event.time > score_time) {
+                if (event.event_type == EventType::CheckFailed && !if_ci_success) {
                     break;
                 }
-                if (event.event_type == EventType::CheckFailed) {
-                    if_ci_success = false;
-                } else if (event.event_type == EventType::CheckSuccess) {
+                if (event.event_type == EventType::CheckSuccess) {
                     if_ci_success = true;
-                } else if (event.event_type == EventType::MergeRequestOpen) {
-                    if_merge_closed = false;
-                } else {
+                }
+                if (event.event_type == EventType::MergeRequestOpen && !if_merge_closed) {
+                    break;
+                }
+                if (event.event_type == EventType::MergeRequestClosed) {
                     if_merge_closed = true;
                 }
-            }
-            if (if_ci_success && if_merge_closed) {
-                ans[person].insert(task);
+                if (if_ci_success) {
+                    ans[person].insert(task);
+                    break;
+                }
             }
         }
     }
