fc37d37f213c8b6ce8f2b108a0b17c5f80adf444
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-10 15:33:20 +0300

    scorer rewritten with pointers

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -4,9 +4,17 @@
 #include <set>
 #include <string>
 
+// bool operator<(const std::pair<time_t, EventType>& a, const std::pair<time_t, EventType>& b) {
+//     return a.first < b.first;
+// }
+
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::map<std::string, std::set<std::string>> ans;
-    std::map<std::string, std::map<std::string, std::vector<Event>>> event_list;
+    using Timeline = std::vector<std::pair<const time_t*, const EventType*>>;
+    //    std::map<std::string, std::map<std::string, std::set<std::pair<time_t, EventType>,
+    //    std::greater<std::pair<time_t, EventType>>>>>
+    //        event_list;
+    std::map<std::string, std::map<std::string, Timeline>> event_list;
     std::vector<std::string> people;
     std::vector<std::string> tasks;
     for (const Event& event : events) {
@@ -17,27 +25,31 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
             tasks.push_back(event.task_name);
         }
         if (event.time <= score_time) {
-            event_list[event.student_name][event.task_name].push_back(event);
+            const time_t* event_time = &event.time;
+            const EventType* event_type = &event.event_type;
+            event_list[event.student_name][event.task_name].push_back(std::make_pair(event_time, event_type));
         }
     }
+    using TimelinePair = std::pair<const time_t*, const EventType*>;
     for (const std::string& person : people) {
         for (const std::string& task : tasks) {
             std::sort(event_list[person][task].begin(), event_list[person][task].end(),
-                      [](const Event& a, const Event& b) -> bool { return b.time < a.time; });
+                      [](const TimelinePair& a, const TimelinePair& b) -> bool { return *b.first < *a.first; });
             bool if_ci_success = false;
             bool if_merge_closed = false;
-            for (const Event& event : event_list[person][task]) {
-                if (event.event_type == EventType::CheckFailed && !if_ci_success) {
-                    break;
-                }
-                if (event.event_type == EventType::CheckSuccess) {
+            //            for (auto it = event_list[person][task].begin(); event_list[person][task].end() != it; ++it) {
+            for (const TimelinePair& event : event_list[person][task]) {
+                if (*event.second == EventType::CheckSuccess) {
                     if_ci_success = true;
                 }
-                if (event.event_type == EventType::MergeRequestOpen && !if_merge_closed) {
+                if (*event.second == EventType::CheckFailed && !if_ci_success) {
                     break;
                 }
-                if (event.event_type == EventType::MergeRequestClosed) {
+                if (*event.second == EventType::MergeRequestClosed) {
                     if_merge_closed = true;
+                }
+                if (*event.second == EventType::MergeRequestOpen && !if_merge_closed) {
+                    break;
                 }
                 if (if_ci_success) {
                     ans[person].insert(task);
