dfa380270ba8981a2e52819fe86513b8e3e8c0d3
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-11 15:42:44 +0300

    scorer rewritten with switch

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -27,18 +27,35 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
             bool if_merge_closed = true;
             time_t time_last_merge = 0;
             for (const std::pair<const time_t*, const EventType*>& event : event_list[person][task]) {
-                if (*event.second == EventType::CheckSuccess && *event.first >= time_last_ci) {
-                    if_last_ci_success = true;
-                    time_last_ci = *event.first;
-                } else if (*event.second == EventType::CheckFailed && *event.first >= time_last_ci) {
-                    if_last_ci_success = false;
-                    time_last_ci = *event.first;
-                } else if (*event.second == EventType::MergeRequestClosed && *event.first >= time_last_merge) {
-                    if_merge_closed = true;
-                    time_last_merge = *event.first;
-                } else if (*event.second == EventType::MergeRequestOpen && *event.first >= time_last_merge) {
-                    if_merge_closed = false;
-                    time_last_merge = *event.first;
+                switch (*event.second) {
+                    case EventType::CheckSuccess:
+                        if (*event.first < time_last_ci) {
+                            break;
+                        }
+                        if_last_ci_success = true;
+                        time_last_ci = *event.first;
+                        break;
+                    case EventType::CheckFailed:
+                        if (*event.first < time_last_ci) {
+                            break;
+                        }
+                        if_last_ci_success = false;
+                        time_last_ci = *event.first;
+                        break;
+                    case EventType::MergeRequestOpen:
+                        if (*event.first < time_last_merge) {
+                            break;
+                        }
+                        if_merge_closed = false;
+                        time_last_merge = *event.first;
+                        break;
+                    case EventType::MergeRequestClosed:
+                        if (*event.first < time_last_merge) {
+                            break;
+                        }
+                        if_merge_closed = true;
+                        time_last_merge = *event.first;
+                        break;
                 }
             }
             if (if_last_ci_success && if_merge_closed) {
