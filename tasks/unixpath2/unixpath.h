7a80528dc241a97336af89106674f21e3528c116
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-02-24 14:24:47 +0300

    unixpath2 done

--- tasks/unixpath2/unixpath.h
+++ tasks/unixpath2/unixpath.h
@@ -2,13 +2,19 @@
 
 #include <string>
 #include <string_view>
+#include <vector>
 
 class UnixPath {
 public:
-    UnixPath(std::string_view initial_dir);
+    explicit UnixPath(std::string_view initial_dir);
 
     void ChangeDirectory(std::string_view path);
 
     std::string GetAbsolutePath() const;
     std::string GetRelativePath() const;
+
+private:
+    std::vector<std::string> dir_now;
+    std::vector<std::string> dir_initial;
+    bool is_dir_now_absolute;
 };
