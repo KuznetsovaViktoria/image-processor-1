1fb16c99388c1d19deeec64807e2dd97c9bc853b
Vika Kuznetsova <kuznetzowa.viktoria@gmail.com>
2023-03-07 18:05:56 +0300

    poly *= added

--- tasks/poly/poly.cpp
+++ tasks/poly/poly.cpp
@@ -5,7 +5,7 @@
 
 Poly::Poly(std::initializer_list<int64_t> coefs) {
     int64_t i = 0;
-    for (int64_t x : coefs) {
+    for (int64_t x: coefs) {
         if (x != 0) {
             coefs_[i] = x;
         }
@@ -14,7 +14,7 @@ Poly::Poly(std::initializer_list<int64_t> coefs) {
 }
 
 Poly::Poly(std::initializer_list<std::pair<int64_t, int64_t>> coefs) {
-    for (std::pair<int64_t, int64_t> x : coefs) {
+    for (std::pair<int64_t, int64_t> x: coefs) {
         if (x.second != 0) {
             coefs_[x.first] = x.second;
         }
@@ -23,26 +23,26 @@ Poly::Poly(std::initializer_list<std::pair<int64_t, int64_t>> coefs) {
 
 int64_t Poly::operator()(int64_t k) const {
     int64_t ans = 0;
-    for (std::pair<int64_t, int64_t> a : coefs_) {
+    for (std::pair<int64_t, int64_t> a: coefs_) {
         ans += a.second * static_cast<int64_t>(pow(static_cast<double>(k), static_cast<double>(a.first)));
     }
     return ans;
 }
 
-bool Poly::operator==(const Poly& other) const {
+bool Poly::operator==(const Poly &other) const {
     return coefs_ == other.coefs_;
 }
 
-bool Poly::operator!=(const Poly& other) const {
+bool Poly::operator!=(const Poly &other) const {
     return coefs_ != other.coefs_;
 }
 
-Poly operator+(const Poly& left, const Poly& right) {
+Poly operator+(const Poly &left, const Poly &right) {
     Poly ans;
-    for (std::pair<int64_t, int64_t> a : left.coefs_) {
+    for (std::pair<int64_t, int64_t> a: left.coefs_) {
         ans.coefs_[a.first] = a.second;
     }
-    for (std::pair<int64_t, int64_t> a : right.coefs_) {
+    for (std::pair<int64_t, int64_t> a: right.coefs_) {
         if (ans.coefs_[a.first] + a.second != 0) {
             ans.coefs_[a.first] += a.second;
         } else {
@@ -54,18 +54,18 @@ Poly operator+(const Poly& left, const Poly& right) {
 
 Poly Poly::operator-() const {
     Poly ans;
-    for (std::pair<int64_t, int64_t> a : coefs_) {
+    for (std::pair<int64_t, int64_t> a: coefs_) {
         ans.coefs_[a.first] = -a.second;
     }
     return ans;
 }
 
-Poly operator-(const Poly& left, const Poly& right) {
+Poly operator-(const Poly &left, const Poly &right) {
     return left + (-right);
 }
 
-void Poly::operator+=(const Poly& other) {
-    for (std::pair<int64_t, int64_t> a : other.coefs_) {
+void Poly::operator+=(const Poly &other) {
+    for (std::pair<int64_t, int64_t> a: other.coefs_) {
         if (coefs_[a.first] + a.second != 0) {
             coefs_[a.first] += a.second;
         } else {
@@ -74,8 +74,8 @@ void Poly::operator+=(const Poly& other) {
     }
 }
 
-void Poly::operator-=(const Poly& other) {
-    for (std::pair<int64_t, int64_t> a : other.coefs_) {
+void Poly::operator-=(const Poly &other) {
+    for (std::pair<int64_t, int64_t> a: other.coefs_) {
         if (coefs_[a.first] - a.second != 0) {
             coefs_[a.first] -= a.second;
         } else {
@@ -84,7 +84,7 @@ void Poly::operator-=(const Poly& other) {
     }
 }
 
-std::ostream& operator<<(std::ostream& os, const Poly& poly) {
+std::ostream &operator<<(std::ostream &os, const Poly &poly) {
     if (poly.coefs_.empty()) {
         return os << "y = 0";
     }
@@ -113,12 +113,28 @@ std::ostream& operator<<(std::ostream& os, const Poly& poly) {
     return os << "y = " << ans;
 }
 
-Poly operator*(const Poly& left, const Poly& right) {
+Poly operator*(const Poly &left, const Poly &right) {
     Poly ans;
-    for (std::pair<int64_t, int64_t> a : left.coefs_) {
-        for (std::pair<int64_t, int64_t> b : right.coefs_) {
+    for (std::pair<int64_t, int64_t> a: left.coefs_) {
+        for (std::pair<int64_t, int64_t> b: right.coefs_) {
             ans.coefs_[a.first + b.first] += a.second * b.second;
+            if (ans.coefs_[a.first + b.first] == 0) {
+                ans.coefs_.erase(a.first + b.first);
+            }
         }
     }
     return ans;
+}
+
+void Poly::operator*=(const Poly &other) {
+    std::map<int64_t, int64_t> ans;
+    for (std::pair<int64_t, int64_t> a: other.coefs_) {
+        for (std::pair<int64_t, int64_t> b: coefs_) {
+            ans[a.first + b.first] += a.second * b.second;
+            if (ans[a.first + b.first] == 0) {
+                ans.erase(a.first + b.first);
+            }
+        }
+    }
+    coefs_ = ans;
 }
--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -8,18 +8,31 @@
 class Poly {
 public:
     Poly() = default;
+
     Poly(std::initializer_list<int64_t> coefs);
+
     Poly(std::initializer_list<std::pair<int64_t, int64_t>> coefs);
 
-    bool operator==(const Poly& other) const;
-    bool operator!=(const Poly& other) const;
-    friend Poly operator+(const Poly& left, const Poly& right);
-    friend Poly operator-(const Poly& left, const Poly& right);
+    bool operator==(const Poly &other) const;
+
+    bool operator!=(const Poly &other) const;
+
+    friend Poly operator+(const Poly &left, const Poly &right);
+
+    friend Poly operator-(const Poly &left, const Poly &right);
+
     Poly operator-() const;
-    friend Poly operator*(const Poly& left, const Poly& right);
-    void operator+=(const Poly& other);
-    void operator-=(const Poly& other);
-    friend std::ostream& operator<<(std::ostream& os, const Poly& poly);
+
+    friend Poly operator*(const Poly &left, const Poly &right);
+
+    void operator+=(const Poly &other);
+
+    void operator-=(const Poly &other);
+
+    void operator*=(const Poly &other);
+
+    friend std::ostream &operator<<(std::ostream &os, const Poly &poly);
+
     int64_t operator()(int64_t k) const;
 
 private:
